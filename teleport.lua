0c817ff919e0bba4ef66a7f94e4ff7af48fb695b
wantedxf <128845721+wantedxf@users.noreply.github.com>
2023-04-07 03:19:42 +1000

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -1,3 +1,6 @@
+Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro2/main/ui2"))()
+		local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro/main/ui"))()
+        NotifyLib.prompt('wanteds teleport', 'welcome', 5)
 local savedPosition = nil
 
 function savePosition()
